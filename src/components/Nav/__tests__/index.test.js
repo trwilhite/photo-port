b0da958478896d4d31b3825aad719129d3dd4585
Tamara Wilhite <wilhite.tamara6452@gmail.com>
2022-04-03 21:20:17 -0400

    added photolist component and tests

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -1,35 +1,37 @@
-import React from "react";
-import { render, cleanup } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect";
-import Nav from "..";
+// __tests__/Nav.test.js with hard coded categories
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Nav from '..';
 
 afterEach(cleanup);
 
-describe("Nav component", () => {
-  // baseline test
-  it("renders", () => {
+describe('Nav component', () => {
+  it('renders', () => {
     render(<Nav />);
   });
 
-  // snapshot test
-  it("matches snapshot", () => {
+  it('matches snapshot', () => {
     const { asFragment } = render(<Nav />);
-    // assert value comparison
-  });
-});
 
-describe("emoji is visible", () => {
-  it("inserts emoji into the h2", () => {
+    expect(asFragment()).toMatchSnapshot();
+  });
+})
+
+describe('emoji is visible', () => {
+  it('inserts emoji into the h2', () => {
     const { getByLabelText } = render(<Nav />);
 
-    expect(getByLabelText("camera")).toHaveTextContent("📸");
+    expect(getByLabelText('camera')).toHaveTextContent('📸');
   });
-});
+})
 
-describe("links are visible", () => {
-  it("inserts text into the links", () => {
+describe('links are visible', () => {
+  it('inserts text into the links', () => {
     const { getByTestId } = render(<Nav />);
-    expect(getByTestId("link")).toHaveTextContent("Oh Snap!");
-    expect(getByTestId("about")).toHaveTextContent("About me");
+
+    expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
+    expect(getByTestId('about')).toHaveTextContent('About me');
   });
-});
+
+})
